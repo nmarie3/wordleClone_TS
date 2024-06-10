ec44f3514d989c68a4de5bf87bccf2ec71dbf24b
Natasha <natasha.marie33@gmail.com>
2024-06-10 13:51:18 +0900

    added feature to check if enough letters exist

--- CLI_version/aniWordle.ts
+++ CLI_version/aniWordle.ts
@@ -10,21 +10,33 @@ const randomIndex: number = Math.floor(Math.random() * animalData.length);
 let animalToGuess: string = animalData[randomIndex];
 console.log(animalToGuess)
 
+function countLetters(str: string) {
+    let count: string[] = str.split("");
+    return count.length;
+}
+
+
 function startGame() {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
+
     rl.question('Guess a 5-letter animal: ', (answer: string) => {
-        if (answer === animalToGuess) {
-        console.log('You got it!');
-        rl.close();
-        } else {
-            console.log('Wrong answer, try again!')
+        let letterCount: number = countLetters(answer);
+        if (letterCount !== 5) {
+            console.log('Must be a 5-letter word!');
             rl.close();
-            startGame();
+            startGame(); 
+        } else if (answer === animalToGuess) {
+            console.log('You got it!');
+            rl.close();
+        } else {
+            console.log('Wrong answer, try again!');
+            rl.close();
+            startGame(); 
         }
     });
 }
 
-startGame()
+startGame();
